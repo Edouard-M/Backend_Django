d21970af0415e73ef9057f5eab63e1afcf24d5c2
Edouard-M <edou17@laposte.net>
2023-08-19 23:24:09 +0200

    init index and views

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.defaults import server_error
+from .views import test_view, index
 
 urlpatterns = [
+    path('', index, name="index"),
     path('admin/', admin.site.urls),
+    path('test/', test_view, name="test"),
 ]
---
+++ src/backend/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, "index.html", context={"title": "Backend"})
+
+def test_view(request):
+    return HttpResponse("<h1>Test View</h1>")
