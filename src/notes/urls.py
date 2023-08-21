db383328ad35d3d5e80486f1a7b8cf808ab92284
Edouard Mignien <edou17@laposte.net>
2023-08-22 00:09:13 +0200

    add create note

--- src/notes/urls.py
+++ src/notes/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from .views import notes
+from .views import notes, note, add_note
 
 urlpatterns = [
     path('', notes, name="notes"),
+    path('<int:pk>', note, name="note"),
+    path('add/', add_note, name="add_note"),
 ]
--- src/notes/views.py
+++ src/notes/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .models import Notes
 
 
 def notes(request):
-    return render(request, "notes/index.html", context={"title": "Notes App"})
+    all_notes = Notes.objects.all()
+
+    context = {
+        "title": "Notes App",
+        "all_notes": all_notes,
+    }
+
+    return render(request, "notes/index.html", context=context)
+
+
+def note(request, pk):
+    note = Notes.objects.get(pk=pk)
+    return render(request, "notes/note.html", context={"note": note})
+
+
+def add_note(request):
+
+    if request.method == "POST":
+        title = request.POST.get("title", None)
+        body = request.POST.get("body", None)
+        if title:
+            Notes.objects.create(title=title, body=body)
+
+    return render(request, "notes/add.html", context={})
